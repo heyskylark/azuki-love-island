7ff58174593ed77d355edaa5fa5a8a7f73f820e1
Brandon Feist <brandon.feist@hey.com>
2022-06-12 03:56:00 -0700

    Add sexy beanz

--- frontend/src/pages/Submission.tsx
+++ frontend/src/pages/Submission.tsx
@@ -32,34 +32,39 @@ function Submission() {
     }
 
     return (
-        <div className="lg:w-1/2 lg:pr-4">
-            <h1 className="uppercase font-black text-4xl lg:text-5xl whitespace-pre-line">Azuki Love Island</h1>
-            <h1 className="mb-6 uppercase font-black text-3xl lg:text-4xl whitespace-pre-line">
-                Submissions: {getParticipantCount(participantCount)}<span className="opacity-10"> //</span>
-            </h1>
-            <p className="mb-6 font-mono text-gray-800 lg:text-sm text-xs lg:leading-6 leading-4">
-                64 Azuki battle it out in a heated tournament consisting
-                of 62 match-ups voted on by the community. Who will become
-                the King and Queen of the Island? Let's find out.<br/>
-                <br/>
-                Tell us about yourself. What is your Azuki's story? 
-                Some say personality is more important than looks. 
-                Imagine having both.<br/>
-                <br/>
-                Good luck.
-            </p>
-            <SubmissionForm
-                azukiId={azukiId}
-                twitterHandle={twitterHandle}
-                bio={bio}
-                hobbies={hobbies}
-                loadingSubmission={loadingSubmission}
-                setAzukiId={setAzukiId}
-                setTwitterHandle={setTwitterHandle}
-                setBio={setBio}
-                setHobbies={setHobbies}
-                setLoadingSubmission={setLoadingSubmission}
-            />
+        <div className="flex">
+            <div className="lg:w-1/2 lg:pr-4">
+                <h1 className="uppercase font-black text-4xl lg:text-5xl whitespace-pre-line">Azuki Love Island</h1>
+                <h1 className="mb-6 uppercase font-black text-3xl lg:text-4xl whitespace-pre-line">
+                    Submissions: {getParticipantCount(participantCount)}<span className="opacity-10"> //</span>
+                </h1>
+                <p className="mb-6 font-mono text-gray-800 lg:text-sm text-xs lg:leading-6 leading-4">
+                    64 Azuki battle it out in a heated tournament consisting
+                    of 62 match-ups voted on by the community. Who will become
+                    the King and Queen of the Island? Let's find out.<br/>
+                    <br/>
+                    Tell us about yourself. What is your Azuki's story? 
+                    Some say personality is more important than looks. 
+                    Imagine having both.<br/>
+                    <br/>
+                    Good luck.
+                </p>
+                <SubmissionForm
+                    azukiId={azukiId}
+                    twitterHandle={twitterHandle}
+                    bio={bio}
+                    hobbies={hobbies}
+                    loadingSubmission={loadingSubmission}
+                    setAzukiId={setAzukiId}
+                    setTwitterHandle={setTwitterHandle}
+                    setBio={setBio}
+                    setHobbies={setHobbies}
+                    setLoadingSubmission={setLoadingSubmission}
+                />
+            </div>
+            <div className="lg:w-1/2 hidden lg:block">
+                <img className="fixed bottom-0 right-0 w-1/2" src="images/sexy-beanz.png" alt="Love Island Beanz" />
+            </div>
         </div>
     )
 }
